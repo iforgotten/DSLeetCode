c3369ae286d91f2bad233464048c6ae2677974b5
iforgotten <iforgotten@126.com>
2018-03-03 15:13:19 +0800

    1. Initial Committing and Print "Hello world"

---
+++ LinkedList/LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by AlvinLi on 03/03/2018.
+//  Copyright © 2018 iforgotten. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello World!\r\n");
+    return 0;
+}
