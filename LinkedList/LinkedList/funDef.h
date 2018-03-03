abe2411ee7a115036f1a662e7631733b51d593e7
iforgotten <iforgotten@126.com>
2018-03-03 17:12:16 +0800

    增加就地逆序算法的总结

--- LinkedList/LinkedList/funDef.h
+++ LinkedList/LinkedList/funDef.h
@@ -24,7 +24,10 @@ bool releaseList(PLISTNODE head);
 void printList(PLISTNODE head, const char *name);
 
 // LeetCode 206 Reverse Linked List; Difficul Degree: Easy
-// 已知链表头结点指针head，将链表原地逆置
+// 已知链表头结点指针head，将链表原地逆置,分三步
+// 1. 备份后续节点地址，以便下一次寻址
+// 2. 将当前节点从链表中断开，并填充其指针域（如果reverse链初始化为nullptr，则可以用reverse进行填充）
+// 3. 寻址后续节点
 ListNode* reverseList(ListNode *head);
 
 
