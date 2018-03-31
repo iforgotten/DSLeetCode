e153b734da514924375a7bf66347fa7c9382262a
iforgotten <iforgotten@126.com>
2018-03-31 16:50:46 +0800

    modify 55.can jump

--- Greedy/55.Jump_game.cpp
+++ Greedy/55.Jump_game.cpp
@@ -1,21 +1,23 @@
-bool canJump(vector<int>& nums) {
-	std::vector<int> index;
-	for(int i = 0; i < nums.size(); ++i) {
-		index.push_back(nums[i] + i);
-	}
+bool canJump(std::vector<int>& nums) {
+    bool canJump(vector<int>& nums) {
+        std::vector<int> index;
+        for(int i = 0; i < nums.size(); ++i) {
+            index.push_back(nums[i] + i);
+        }
 
-	int jump = 0;
-	int maxIdx = index[0];
-	for(; jump < index.size(); ++i) {
-		if(jump > maxIdx) {
-			break;
-		}
-		if(index[i] > maxIdx) {
-			maxIdx = index[i];
-		}
-	}
+        int jump = 0;
+        int maxIdx = index[0];
+        for(; jump < index.size(); ++jump) {
+            if(jump > maxIdx) {
+                break;
+            }
+            if(index[jump] > maxIdx) {
+                maxIdx = index[jump];
+            }
+        }
 
-	if(jump >= nums.size()) 
-		return true;
-	return false;
+        if(jump >= nums.size()) 
+            return true;
+        return false;
+    }
 }
