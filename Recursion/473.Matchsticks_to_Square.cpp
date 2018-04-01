5c723c0b5e6b78d05112c83dfce97247f704cd8c
iforgotten <iforgotten@126.com>
2018-04-01 13:22:07 +0800

    add new method to 473.

--- Recursion/473.Matchsticks_to_Square.cpp
+++ Recursion/473.Matchsticks_to_Square.cpp
@@ -39,3 +39,54 @@ public:
 		return robot(0, nums, sum/4, bucket);
     }
 };
+
+class Solution {
+public:
+    bool makesquare(vector<int>& nums) {
+		if(nums.size() < 4)
+			return false;
+		
+		int sum = 0;
+		for(int i = 0; i < nums.size(); ++i) {
+			sum += nums[i];
+		}
+        
+		if(sum % 4 != 0) {
+			return false;
+		}
+		
+		int target = sum / 4;
+		std::vector<int> subset;
+		std::vector<int> halfset;
+		int all = 1 << nums.size();
+				
+		for(int i = 0; i < all; ++i) {
+			int sum = 0;
+			for(int j = 0; j < nums.size(); ++j) {
+				if(i & (1 << j)) {
+					sum += nums[j];
+				}
+			}
+			if(sum == target) {
+				subset.push_back(i);
+			}
+		}
+		
+		for(int i = 0; i < subset.size(); ++i) {
+			for(int j = i + 1 ; j < subset.size(); ++j) {
+				if((subset[i] & subset[j]) == 0) {
+					halfset.push_back(subset[i] | subset[j]);
+				}
+			}
+		}
+		
+		for(int i = 0; i < halfset.size(); ++i) {
+			for(int j = i + 1; j < halfset.size(); j++) {
+				if((halfset[i] & halfset[j]) == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+};
