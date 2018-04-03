a73b8279f1363e71c3e1bd7edd3bf0a105b285e8
iforgotten <iforgotten@126.com>
2018-04-03 17:56:54 +0800

    add new method to 23

--- Conquer/23.Merge_K_Sorted_Lists.cpp
+++ Conquer/23.Merge_K_Sorted_Lists.cpp
@@ -35,3 +35,49 @@ public:
 		return nodeVec[0];
     }
 };
+
+class Solution {
+	ListNode* merge(ListNode* l1, ListNode* l2) {
+    	ListNode temp_head(0);
+    	ListNode *pre = &temp_head;
+    	while (l1 && l2){
+	    	if (l1->val < l2->val){
+	    		pre->next = l1;
+	    		l1 = l1->next;
+	    	}
+	    	else{
+	    		pre->next = l2;
+	    		l2 = l2->next;
+	    	}
+	    	pre = pre->next;
+	    }
+	    if (l1){
+    		pre->next = l1;
+    	}
+    	if (l2){
+	    	pre->next = l2;
+	    }
+        return temp_head.next;
+	}
+public:
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		if(lists.size() == 0) 
+			return NULL;
+		else if(lists.size() == 1) 
+			return lists[0];
+		
+		std::vector<ListNode*> subset1;
+		std::vector<ListNode*> subset2;
+		
+		int mid = lists.size() >> 1;
+		for(int i = 0; i < mid; ++i) {
+			subset1.push_back(lists[i]);
+		}
+		for(int i = mid; i < lists.size(); ++i) {
+			subset2.push_back(lists[i]);
+		}
+		ListNode* l1 = mergeKLists(subset1);
+		ListNode* l2 = mergeKLists(subset2);
+		return merge(l1, l2);
+	}
+};
